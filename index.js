a0840a4587399d8476cfb4190fa749e14ba41e08
Cristina SG <cristinasanchezg@gmail.com>
2022-11-15 13:20:59 -0600

    creacion de endpoint

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+server.get("/", (request, response) => {
+    response.send("API V1.0");
+});
+
 server.listen(3000, () => {
     console.log("Servidor iniciado en el puerto 3000")
 });
