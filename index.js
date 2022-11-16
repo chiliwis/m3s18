5f73370e294e800621704392f0ba588620785a38
Cristina SG <cristinasanchezg@gmail.com>
2022-11-15 18:32:37 -0600

    Get y post en postman

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const PORT = 3000;
+
+let platillos = [
+    { nombre: 'Tacos', precio: 50 },
+    { nombre: 'Mole', precio: 100 }
+];
+
+
+
 server.get("/", (request, response) => {
     response.send("API V1.0");
 });
 
-server.listen(3000, () => {
+server.get("/platillos", (request, response) => {
+    response.json(
+        {
+            data: platillos,
+            count: platillos.length,
+            mensaje: "Platillos obtenidos correectamente"
+        }
+    );
+});
+
+server.post("/platillos", (request, response) => {
+    response.json(
+        {
+            data: request.body,
+            count: platillos.length,
+            mensaje: "Entro a la funcion de agregar platillo"
+        }
+    );
+});
+
+server.listen(PORT, () => {
     console.log("Servidor iniciado en el puerto 3000")
 });
